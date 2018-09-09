564d2d36ce49af7dd8976cc7a4fcf8faf25f2768
梁建溢 <liangjianyi.com@outlook.com>
2018-09-09 09:44:20 -0400

    Changesets-01

--- MasturbationRecorder/MainPage.xaml.cs
+++ MasturbationRecorder/MainPage.xaml.cs
@@ -13,18 +13,50 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+namespace MasturbationRecorder {
+	using Debug = System.Diagnostics.Debug;
 
-namespace MasturbationRecorder
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	public sealed partial class MainPage : Page {
+		private Window _window = Window.Current;
+
+		public MainPage() {
+			this._window.SizeChanged += Current_SizeChanged;
+			this.InitializeComponent();
+			Debug.WriteLine($"{this._window.Bounds.Width} , {this._window.Bounds.Height}");
+			CalculateDateTime();
+			this.RectanglesLayout();
+		}
+
+		private static void CalculateDateTime() {
+			DateTime now = DateTime.Now;
+			DateTime today = new DateTime(now.Year, now.Month, now.Day);
+			DateTime todayOfLastyear = new DateTime(today.Year - 1, today.Month, today.Day);
+			Debug.WriteLine($"today is {today.DayOfWeek}");
+			Debug.WriteLine($"todayOfLastyear is {todayOfLastyear.DayOfWeek}");
+			TimeSpan fuck = today - todayOfLastyear;
+			Debug.WriteLine(fuck);
+			Debug.WriteLine(fuck.Days);
+			Debug.WriteLine(fuck.TotalDays);
+			Debug.WriteLine($"Week: {((uint)(fuck.TotalDays / 7))}   Remain days: {fuck.TotalDays % 7}");
+		}
+
+		private void RectanglesLayout() {
+			int columnDistance = 10;
+			int rowDistance = 10;
+
+		}
+
+		private void Current_SizeChanged(object sender, Windows.UI.Core.WindowSizeChangedEventArgs e) {
+			Debug.WriteLine($"{this._window.Bounds.Width} , {this._window.Bounds.Height}");
+		}
+
+		private static void SetGridLayout(Grid grid, int rowCount = 0, int columnCount = 0) {
+			for (int c = 0; c < columnCount; c++) {
+				grid.ColumnDefinitions.Add(new ColumnDefinition());
+			}
+			for (int r = 0; r < rowCount; r++) {
+				grid.RowDefinitions.Add(new RowDefinition());
+			}
+		}
+	}
 }
