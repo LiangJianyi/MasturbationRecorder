d9b49b59683075a5eb5410e1dca2646f3511fb4d
梁建溢 <liangjianyi.com@outlook.com>
2019-03-30 05:14:33 -0400

    Changesets-43

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Permissions;
+using System.Reflection;
+
+namespace ConsoleApp1 {
+	class Program {
+		static void Main(string[] args) {
+			const string path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder\MasturbationRecorder\bin\x64\Debug\MasturbationRecorder.exe";
+			FileIOPermission permission = new FileIOPermission(
+				FileIOPermissionAccess.Read,
+				@"C:\Users\a124p\Documents\GitHub\MasturbationRecorder");
+			permission.Demand();
+			Assembly assembly = Assembly.LoadFrom(path);
+			var methodInfo = assembly.GetType("MasturbationRecorder.DatetimeParser").GetMethod("StringToUInt16", BindingFlags.NonPublic | BindingFlags.Static);
+			Console.WriteLine(methodInfo.Invoke(null, new object[] { "mon" }));
+			Console.WriteLine(methodInfo.Invoke(null, new object[] { "feb" }));
+			Console.WriteLine(methodInfo.Invoke(null, new object[] { "mar" }));
+			Console.WriteLine(methodInfo.Invoke(null, new object[] { "apr" }));
+			Console.WriteLine(methodInfo.Invoke(null, new object[] { "may" }));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ UnitTestMasturbationRecorder/DatetimeParserTest.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MasturbationRecorder;
+using System.Reflection;
+
+namespace UnitTestMasturbationRecorder {
+	[TestClass]
+	public class DatetimeParserTest {
+		[TestMethod]
+		public void StringToUInt16Test() {
+			MethodInfo stringToUInt16 = typeof(DatetimeParser).GetMethod("StringToUInt16", BindingFlags.NonPublic | BindingFlags.Static); ;
+
+			ushort[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+			ushort[] actual = {
+				(ushort)stringToUInt16.Invoke(null, new object[] { "mon" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "feb" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "mar" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "apr" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "may" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "jun" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "jul" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "aug" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "sep" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "oct" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "nov" }),
+				(ushort)stringToUInt16.Invoke(null, new object[] { "dec" }),
+			};
+
+			CollectionAssert.AreEquivalent(expected, actual);
+		}
+	}
+}
