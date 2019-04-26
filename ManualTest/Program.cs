c958f97a27896524f6c259fbe84f49d7edc53788
梁建溢 <liangjianyi.com@outlook.com>
2019-04-26 11:52:16 -0400

    Changesets-55

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -1,23 +1,46 @@
 ﻿using System;
 using System.Security.Permissions;
 using System.Reflection;
+using System.Collections.Generic;
 
-namespace ConsoleApp1 {
+/// <summary>
+/// 该项目只要用于输出log等测试数据，为 MasturbationRecorder 解决方案下的项目
+/// 进行人工数据比对。
+/// </summary>
+namespace ManualTest {
 	class Program {
 		static void Main(string[] args) {
-			const string path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder\MasturbationRecorder\bin\x64\Debug\MasturbationRecorder.exe";
-			FileIOPermission permission = new FileIOPermission(
-				FileIOPermissionAccess.Read,
-				@"C:\Users\a124p\Documents\GitHub\MasturbationRecorder");
-			permission.Demand();
-			Assembly assembly = Assembly.LoadFrom(path);
-			var methodInfo = assembly.GetType("MasturbationRecorder.DatetimeParser").GetMethod("StringToUInt16", BindingFlags.NonPublic | BindingFlags.Static);
-			Console.WriteLine(methodInfo.Invoke(null, new object[] { "mon" }));
-			Console.WriteLine(methodInfo.Invoke(null, new object[] { "feb" }));
-			Console.WriteLine(methodInfo.Invoke(null, new object[] { "mar" }));
-			Console.WriteLine(methodInfo.Invoke(null, new object[] { "apr" }));
-			Console.WriteLine(methodInfo.Invoke(null, new object[] { "may" }));
-			Console.ReadKey();
-		}
-	}
+            const string MasturbationRecorder_exe_path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder\MasturbationRecorder\bin\x64\Debug\MasturbationRecorder.exe";
+            const string MasturbationRecorder_path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder";
+
+            FileIOPermission permission = new FileIOPermission(
+                FileIOPermissionAccess.Read,
+                MasturbationRecorder_path
+            );
+            permission.Demand();
+            Assembly assembly = Assembly.LoadFrom(MasturbationRecorder_exe_path);
+            MasturbationRecorder_DatetimeParser_StringToUInt16_Test(assembly);
+
+
+            Console.ReadKey();
+        }
+
+        private static void MasturbationRecorder_DatetimeParser_StringToUInt16_Test(Assembly assembly) {
+            var methodInfo = assembly.GetType("MasturbationRecorder.DatetimeParser").GetMethod("StringToUInt16", BindingFlags.NonPublic | BindingFlags.Static);
+            Console.WriteLine(methodInfo.Invoke(null, new object[] { "mon" }));
+            Console.WriteLine(methodInfo.Invoke(null, new object[] { "feb" }));
+            Console.WriteLine(methodInfo.Invoke(null, new object[] { "mar" }));
+            Console.WriteLine(methodInfo.Invoke(null, new object[] { "apr" }));
+            Console.WriteLine(methodInfo.Invoke(null, new object[] { "may" }));
+        }
+
+        private static void MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff(Assembly assembly) {
+            var methodInfo = assembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("GroupDateTimesDiff", BindingFlags.Public | BindingFlags.Static);
+            Type statistTotalByDateTime = assembly.GetType("MasturbationRecorder.StatistTotalByDateTime");
+            /*
+             * 想办法如何用反射实例化泛型类
+             */
+            LinkedList<StatistTotalByDateTime> dateTimes = null;
+        }
+    }
 }
