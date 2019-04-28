317d5622e8a87d964a43f694d358d2bf2b8d5bfd
梁建溢 <liangjianyi.com@outlook.com>
2019-04-28 03:18:23 -0400

    Update Program.cs

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -11,6 +11,7 @@ namespace ManualTest {
     class Program {
         static void Main(string[] args) {
             const string MasturbationRecorder_exe_path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder\MasturbationRecorder\bin\x64\Debug\MasturbationRecorder.exe";
+            const string MasturbationRecorderTest_path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder\MasturbationRecorderTest\bin\x64\Debug\MasturbationRecorderTest.exe";
             const string MasturbationRecorder_path = @"C:\Users\a124p\Documents\GitHub\MasturbationRecorder";
 
             FileIOPermission permission = new FileIOPermission(
@@ -18,9 +19,10 @@ namespace ManualTest {
                 MasturbationRecorder_path
             );
             permission.Demand();
-            Assembly assembly = Assembly.LoadFrom(MasturbationRecorder_exe_path);
-            MasturbationRecorder_DatetimeParser_StringToUInt16_Test(assembly);
-            MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff(assembly);
+            Assembly MasturbationRecorderAssembly = Assembly.LoadFrom(MasturbationRecorder_exe_path);
+            Assembly MasturbationRecorderTestAssembly = Assembly.LoadFrom(MasturbationRecorderTest_path);
+            MasturbationRecorder_DatetimeParser_StringToUInt16_Test(MasturbationRecorderAssembly);
+            MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff_Test(MasturbationRecorderAssembly, MasturbationRecorderTestAssembly);
 
             Console.ReadKey();
         }
@@ -34,20 +36,55 @@ namespace ManualTest {
             Console.WriteLine(methodInfo.Invoke(null, new object[] { "may" }));
         }
 
-        private static void MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff(Assembly assembly) {
-            var methodInfo = assembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("GroupDateTimesDiff", BindingFlags.Public | BindingFlags.Static);
-            Type statistTotalByDateTime = assembly.GetType("MasturbationRecorder.StatistTotalByDateTime");
-            Type[] genericArgs = { statistTotalByDateTime };
-            Type likType = typeof(LinkedList<>);
-            object o = Activator.CreateInstance(likType.MakeGenericType(genericArgs));
-            Console.WriteLine(o.GetType().InvokeMember(
-                name: "First",
-                invokeAttr: BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance,
-                target: o,
+        private static void MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff_Test(Assembly mastAssembly, Assembly mastTestAssembly) {
+            var groupDateTimesByDiffInfo = mastAssembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("GroupDateTimesByDiff", BindingFlags.Public | BindingFlags.Static);
+            var linesConvertToStatistTotalByDateTimesInfo = mastAssembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("LinesConvertToStatistTotalByDateTimes", BindingFlags.Public | BindingFlags.Static);
+            var testTextLineInfo = mastTestAssembly.GetType("MasturbationRecorderTest.CommonTestResource").GetProperty("TestTextLine");
+            var testTextLine = testTextLineInfo.GetValue(null);
+            var dateTimes = linesConvertToStatistTotalByDateTimesInfo.Invoke(
+                obj: null,
+                invokeAttr: BindingFlags.Static | BindingFlags.InvokeMethod | BindingFlags.Public,
                 binder: null,
-                args: null
-            ));
-            //LinkedList<StatistTotalByDateTime> dateTimes = null;
+                culture: null,
+                parameters: new object[] { testTextLine }
+            );
+            //Type statistTotalByDateTime = mastAssembly.GetType("MasturbationRecorder.StatistTotalByDateTime");
+            //Type[] genericArgs = { statistTotalByDateTime };
+            //Type likType = typeof(LinkedList<>);
+            //object o = Activator.CreateInstance(likType.MakeGenericType(genericArgs));
+            var res = groupDateTimesByDiffInfo.Invoke(
+                obj: null,
+                invokeAttr: BindingFlags.Static | BindingFlags.InvokeMethod | BindingFlags.Public,
+                binder: null,
+                culture: null,
+                parameters: new object[] { dateTimes }
+            );
+            Console.WriteLine(res);
+        }
+
+        void fuck() {
+            //o = new LinkedList<int>();
+            //o.GetType().InvokeMember(
+            //    name: "AddLast",
+            //    invokeAttr: BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Instance,
+            //    target: o,
+            //    binder: null,
+            //    args: new object[] { 11223344 }
+            //);
+            //o = o.GetType().InvokeMember(
+            //    name: "First",
+            //    invokeAttr: BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance,
+            //    target: o,
+            //    binder: null,
+            //    args: null
+            //);
+            //Console.WriteLine(o.GetType().InvokeMember(
+            //    name: "Value",
+            //    invokeAttr: BindingFlags.GetProperty | BindingFlags.Public | BindingFlags.Instance,
+            //    target: o,
+            //    binder: null,
+            //    args: null
+            //));
         }
     }
 }
