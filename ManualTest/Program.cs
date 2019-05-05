c6f019c9179f08f646cdbea301fabaceb40d5c1d
梁建溢 <liangjianyi.com@outlook.com>
2019-05-05 12:48:49 -0400

    Update Program.cs

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -2,6 +2,7 @@
 using System.Security.Permissions;
 using System.Reflection;
 using System.Collections.Generic;
+using System.Linq;
 
 /// <summary>
 /// 该项目只要用于输出log等测试数据，为 MasturbationRecorder 解决方案下的项目
@@ -19,6 +20,7 @@ namespace ManualTest {
                 MasturbationRecorder_path
             );
             permission.Demand();
+
             Assembly MasturbationRecorderAssembly = Assembly.LoadFrom(MasturbationRecorder_exe_path);
             Assembly MasturbationRecorderTestAssembly = Assembly.LoadFrom(MasturbationRecorderTest_path);
             MasturbationRecorder_DatetimeParser_StringToUInt16_Test(MasturbationRecorderAssembly);
@@ -36,56 +38,75 @@ namespace ManualTest {
             Console.WriteLine(methodInfo.Invoke(null, new object[] { "may" }));
         }
 
+        /*
+         * 该方法使用的反射技术参见： https://docs.microsoft.com/zh-cn/dotnet/framework/reflection-and-codedom/how-to-examine-and-instantiate-generic-types-with-reflection
+         */
         private static void MasturbationRecorder_MainPageViewModel_GroupDateTimesDiff_Test(Assembly mastAssembly, Assembly mastTestAssembly) {
             var groupDateTimesByDiffInfo = mastAssembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("GroupDateTimesByDiff", BindingFlags.Public | BindingFlags.Static);
-            var linesConvertToStatistTotalByDateTimesInfo = mastAssembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("LinesConvertToStatistTotalByDateTimes", BindingFlags.Public | BindingFlags.Static);
-            var testTextLineInfo = mastTestAssembly.GetType("MasturbationRecorderTest.CommonTestResource").GetProperty("TestTextLine");
-            var testTextLine = testTextLineInfo.GetValue(null);
-            var dateTimes = linesConvertToStatistTotalByDateTimesInfo.Invoke(
-                obj: null,
-                invokeAttr: BindingFlags.Static | BindingFlags.InvokeMethod | BindingFlags.Public,
-                binder: null,
-                culture: null,
-                parameters: new object[] { testTextLine }
-            );
-            for (var node = dateTimes.GetType().InvokeMember("First", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, null, dateTimes, null);
+            object testData_linkedlist_dateTimes = MakeTestDataObject(mastAssembly, mastTestAssembly);
+            for (var node = testData_linkedlist_dateTimes.GetType().InvokeMember(
+                                                            name: "First", 
+                                                            invokeAttr: BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, 
+                                                            binder: null, 
+                                                            target: testData_linkedlist_dateTimes, 
+                                                            args: null);
                 node != null;
                 node = node.GetType().InvokeMember("Next", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, null, node, null)
                 ) {
-                Console.WriteLine(node.GetType().InvokeMember("Value", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, null, node, null));
 
+                // 把 StatistTotalByDateTime 作为 LinkedList 的泛型参数
                 Type statistTotalByDateTime = mastAssembly.GetType("MasturbationRecorder.StatistTotalByDateTime");
-                Type[] genericArgs = { statistTotalByDateTime };
-                Type likType = typeof(LinkedList<>);
-                object tempDatetimesLik = Activator.CreateInstance(likType.MakeGenericType(genericArgs));
+                Type[] genericArgsOfDateTimes = { statistTotalByDateTime };
 
+                // 实例化 LinkedList<StatistTotalByDateTime> tempLikDatetimes
+                Type likType = typeof(LinkedList<>);
+                object tempLikDateTimes = Activator.CreateInstance(likType.MakeGenericType(genericArgsOfDateTimes));
+
+                // 实例化 LinkedListNode<StatistTotalByDateTime>
                 Type likNodeType = typeof(LinkedListNode<>);
                 object tempLikNode = Activator.CreateInstance(
-                    type: likNodeType.MakeGenericType(genericArgs),
+                    type: likNodeType.MakeGenericType(genericArgsOfDateTimes),
                     args: node.GetType().InvokeMember("Value", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, null, node, null)
                 );
 
                 Console.WriteLine(tempLikNode.GetType().InvokeMember("Value", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty, null, tempLikNode, null));
 
-                #region 临时封装一个单元素链表
-                Type statistTotalByDateTimeOfLikInfo = tempDatetimesLik.GetType();
-                statistTotalByDateTimeOfLikInfo.InvokeMember("AddLast", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance, null, tempDatetimesLik, new object[] { tempLikNode });
-                #endregion
+                // 临时封装一个单元素链表
+                Type statistTotalByDateTimeOfLikInfo = tempLikDateTimes.GetType();
+                statistTotalByDateTimeOfLikInfo.InvokeMember("AddLast", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance, null, tempLikDateTimes, new object[] { tempLikNode });
 
-                dynamic res = groupDateTimesByDiffInfo.Invoke(null, new object[] { tempDatetimesLik });
-                Console.WriteLine($"Called GroupDateTimesByDiff: {res[0].Ordinal} {res[0].Diff} {res[0].StaticsList}");
+                var listOfTuple = groupDateTimesByDiffInfo.Invoke(null, new object[] { tempLikDateTimes });
+                Console.WriteLine(listOfTuple.GetType().Name);
+                // How called the generic method with List.First<(ulong Ordinal, long Diff, SortedList<ulong, StatistTotalByDateTime> StaticsList)>()
+                IEnumerable<MethodInfo> firstInfo = typeof(Enumerable)
+                                        .GetMethods(BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Static)
+                                        .Where(m => m.IsGenericMethod &&
+                                               m.GetParameters().Length == 1 &&
+                                               m.GetParameters()[0].ParameterType.GetGenericTypeDefinition().IsEquivalentTo(typeof(IEnumerable<>)));
+                var listOfInt_First = firstInfo.First().MakeGenericMethod(new Type[] { listOfTuple.GetType().GetGenericArguments().First() });
+                Console.WriteLine($"Called GroupDateTimesByDiff: {listOfInt_First.Invoke(null, new object[] { listOfTuple })}");
+
             }
             Console.WriteLine();
             Console.WriteLine();
+        }
 
-            //var res = groupDateTimesByDiffInfo.Invoke(
-            //    obj: null,
-            //    invokeAttr: BindingFlags.Static | BindingFlags.InvokeMethod | BindingFlags.Public,
-            //    binder: null,
-            //    culture: null,
-            //    parameters: new object[] { dateTimes }
-            //);
-            //Console.WriteLine(res);
+        /// <summary>
+        /// 从 MasturbationRecorder.MainPageViewModel 的 LinesConvertToStatistTotalByDateTimes
+        /// 方法中提取测试数据并生成 StatistTotalByDateTimes 实例用于测试。
+        /// </summary>
+        /// <param name="mastAssembly">主体项目</param>
+        /// <param name="mastTestAssembly">当前测试项目</param>
+        /// <returns>返回一个LinkedList<StatistTotalByDateTime>实例</returns>
+        private static object MakeTestDataObject(Assembly mastAssembly, Assembly mastTestAssembly) {
+            var linesConvertToStatistTotalByDateTimesInfo = mastAssembly.GetType("MasturbationRecorder.MainPageViewModel").GetMethod("LinesConvertToStatistTotalByDateTimes", BindingFlags.Public | BindingFlags.Static);
+            var testTextLineInfo = mastTestAssembly.GetType("MasturbationRecorderTest.CommonTestResource").GetProperty("TestTextLine");
+            var testTextLine = testTextLineInfo.GetValue(null);
+            var linkedlist_dateTimes = linesConvertToStatistTotalByDateTimesInfo.Invoke(
+                obj: null,
+                parameters: new object[] { testTextLine }
+            );
+            return linkedlist_dateTimes;
         }
 
         void fuck() {
