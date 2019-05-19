4c34abb6869503253de9d37e35648416df3fe540
梁建溢 <liangjianyi.com@outlook.com>
2019-05-19 07:02:23 -0400

    Update DatetimeParserTest.cs

--- MasturbationRecorderTest/DatetimeParserTest.cs
+++ MasturbationRecorderTest/DatetimeParserTest.cs
@@ -19,7 +19,7 @@ namespace MasturbationRecorderTest {
 
             ushort[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
             ushort[] actual = {
-                (ushort)stringToUInt16.Invoke(null, new object[] { "mon" }),
+                (ushort)stringToUInt16.Invoke(null, new object[] { "jan" }),
                 (ushort)stringToUInt16.Invoke(null, new object[] { "feb" }),
                 (ushort)stringToUInt16.Invoke(null, new object[] { "mar" }),
                 (ushort)stringToUInt16.Invoke(null, new object[] { "apr" }),
