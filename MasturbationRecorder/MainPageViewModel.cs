b6844bf66d94ff565eca07646f4e07711bdd23ba
梁建溢 <liangjianyi.com@outlook.com>
2019-06-04 06:51:49 -0400

    Update MainPageViewModel.cs

--- MasturbationRecorder/MainPageViewModel.cs
+++ MasturbationRecorder/MainPageViewModel.cs
@@ -11,9 +11,9 @@ namespace MasturbationRecorder {
     /// 控制 MainPage.xaml 的视图逻辑
     /// </summary>
     static class MainPageViewModel {
-        internal static Windows.UI.Color LightGray => new Windows.UI.Color() { A = 255, R = 199, G = 203, B = 200 };
+        internal static Windows.UI.Color LightGray => new Windows.UI.Color() { A = 255, R = 214, G = 218, B = 215 };
         internal static Windows.UI.Color OneLevelColor => new Windows.UI.Color() { A = 255, R = 203, G = 229, B = 146 };
-        internal static Windows.UI.Color TwoLevelColor => new Windows.UI.Color() { A = 255, R = 142, G = 201, B = 117 };
+        internal static Windows.UI.Color TwoLevelColor => new Windows.UI.Color() { A = 255, R = 142, G = 230, B = 107 };
         internal static Windows.UI.Color ThreeLevelColor => new Windows.UI.Color() { A = 255, R = 78, G = 154, B = 67 };
         internal static Windows.UI.Color FourLevelColor => new Windows.UI.Color() { A = 255, R = 11, G = 110, B = 0 };
         internal static Windows.UI.Color FiveLevelColor => new Windows.UI.Color() { A = 255, R = 0, G = 58, B = 6 };
