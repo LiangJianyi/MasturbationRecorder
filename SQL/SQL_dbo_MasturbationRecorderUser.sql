24992dea49bfa9d4cdb27851801c8769597fde39
梁建溢 <liangjianyi.com@outlook.com>
2019-08-11 13:56:57 -0400

    Update SQL_dbo_MasturbationRecorderUser.sql

--- SQL/SQL_dbo_MasturbationRecorderUser.sql
+++ SQL/SQL_dbo_MasturbationRecorderUser.sql
@@ -1,10 +1,22 @@
 ﻿IF (NOT EXISTS(SELECT TABLE_SCHEMA, TABLE_NAME FROM INFORMATION_SCHEMA.TABLES 
 				WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'MasturbationRecorderUser'))
 BEGIN
-CREATE TABLE dbo.MasturbationRecorderUser(
-	UserId INT PRIMARY KEY IDENTITY(0,1),
-	UserName NCHAR(128) CHECK(LEN(UserName) > 0) UNIQUE NOT NULL,
-	Password CHAR(256) CHECK(LEN(Password) >= 8) NOT NULL,
-	PersonData image
-)
+	CREATE TABLE dbo.MasturbationRecorderUser(
+		UserId INT PRIMARY KEY IDENTITY(0,1),
+		UserName NCHAR(128) CHECK(LEN(UserName) > 0) UNIQUE NOT NULL,
+		Password CHAR(256) CHECK(LEN(Password) >= 8) NOT NULL,
+		PersonData image
+	)
+END
+ELSE
+BEGIN
+	IF (NOT EXISTS(SELECT UserName FROM dbo.MasturbationRecorderUser))
+	BEGIN
+		insert into dbo.MasturbationRecorderUser (UserName,Password) 
+		VALUES('Janyee Liang', 'ddbc4578'),
+		      ('Phoebe Kilminster', 'phoebe001234'),
+		      ('Reina Stamm', 'edc54gbyuio'),
+		      ('Lsm', '1234567890AXC'),
+		      ('Han Solo', 'Wow!!Shit007')
+	END
 END
